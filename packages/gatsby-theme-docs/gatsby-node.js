0716c6040525d6e12c95ddef69ebc420889d3339
becem-dev <you@example.com>
2021-07-09 21:31:24 +0100

    add prebootstrap

---
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -0,0 +1,12 @@
+const withDefaults = require('./utils/default-options');
+const path = require('path');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+exports.onPreBootstrap = ({ store }, options) => {
+  const { program } = store.getState();
+  const { contentPath } = withDefaults(options);
+  const dir = path.join(program.directory, contentPath);
+  if (!fs.existsSync(dir)) {
+    mkdirp.sync(dir);
+  }
+};
